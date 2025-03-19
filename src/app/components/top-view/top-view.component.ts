6169cdf9a053f7a7f79688930619f18bca3b1061
NielsLazaroms <niels.lazaroms@live.nl>
2025-03-19 19:49:02 +0100

    Add 3D animation to top-view component
    Introduced GSAP-powered 3D animations for elements in the top-view component, enhancing visual engagement. Updated SCSS to include perspective and transform-style properties for 3D effects. This improves the overall user experience with smoother and more dynamic interactions.

--- src/app/components/top-view/top-view.component.ts
+++ src/app/components/top-view/top-view.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-top-view',
@@ -18,6 +19,43 @@ export class TopViewComponent implements OnInit {
 
   ngOnInit() {
     this.typeEffect();
+    document.addEventListener("DOMContentLoaded", function() {
+      const tl = gsap.timeline();
+
+      tl.fromTo(
+        ".top-view__center",
+        { scale: 0.1, z: -500 },
+        { duration: 2, scale: 1, z: 0, ease: "power3.out" }
+      );
+
+      tl.fromTo(
+        ".top-view__small-circle--left",
+        { x: -500, y: -500 },
+        { duration: 1, x: 0, y: 0, ease: "power3.out" },
+        "<"
+
+
+      );
+
+      tl.fromTo(
+        ".top-view__small-circle--right",
+        { x: 500, y: -500 },
+        { duration: 1, x: 0, y: 0, ease: "power3.out" },
+        "<"
+      );
+      tl.fromTo(
+        ".top-view__small-circle--bottom-left",
+        { x: -500},
+        { duration: 1, x: 0, y: 0, ease: "power3.out" },
+        "<"
+      );
+      tl.fromTo(
+        ".top-view__small-circle--bottom-right",
+        { x: 500},
+        { duration: 1, x: 0, y: 0, ease: "power3.out" },
+        "<"
+      );
+    });
   }
 
   typeEffect() {
